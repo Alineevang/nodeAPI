327275bccd84d8ae91ad70696f21cd0c095052d9
Alineevang <aline.evangelista@aluno.senai.br>
2023-11-09 14:58:37 -0300

    feat: create server

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express'; //serve para escutar as requisisções
+import { config } from 'dotenv'; //serve para ler o arquivo .env
+
+config(); //configura o dotenv
+
+const port = process.env.PORT || 5000; //pega a porta do .env ou usa a 3000
+
+const app = express(); //cria o servidor
+app.use(express.json()); //faz o express entender json
+
+app.get('/', (req, res) => {
+    // Rota inicial
+    res.status(200).json({ message: 'Hello World!' });
+    });
+
+app.listen(port, () => {
+    // Inicia o servidor
+    console.log(`Server is running on port http://localhost: ${port}`);
+    });
